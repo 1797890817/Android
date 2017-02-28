2432f83416090e7356d249e0200b0b297ea06f9b
dbdu <1797890817@qq.com>
2017-02-28 11:15:24 +0800

    add UIBestProctice

---
+++ UIBestPractice/app/src/main/java/com/ddb/uibestpractice/Msg.java
@@ -0,0 +1,26 @@
+package com.ddb.uibestpractice;
+
+/**
+ * Created by deepin on 17-2-28.
+ */
+
+public class Msg {
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SENT = 1;
+
+    private String content;
+    private int type;
+
+    public Msg(String content,int type){
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent(){
+        return  this.content;
+    }
+
+    public int getType(){
+        return this.type;
+    }
+}
