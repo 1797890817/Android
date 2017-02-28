9c003c6479e30f2abd3cd9e2ed33a5df9477b6a8
dbdu <1797890817@qq.com>
2017-02-28 22:10:17 +0800

    add FragmentTest

---
+++ FragmentTest/app/src/main/java/com/ddb/fragmenttest/AnotherRightFragment.java
@@ -0,0 +1,22 @@
+package com.ddb.fragmenttest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by deepin on 17-2-28.
+ */
+
+public class AnotherRightFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.another_right_fragment,container,false);
+        return view;
+    }
+
+
+}
