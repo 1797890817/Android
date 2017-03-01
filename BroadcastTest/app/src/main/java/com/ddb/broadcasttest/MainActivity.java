4f638969d92d35db651b149a679a07ed48cec3ce
dbdu <1797890817@qq.com>
2017-03-01 21:59:37 +0800

    add BroadcastTest

---
+++ BroadcastTest/app/src/main/java/com/ddb/broadcasttest/MainActivity.java
@@ -0,0 +1,73 @@
+package com.ddb.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+    private NetworkChangeReceiver networkChangeReceiver;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //使用按钮主动发送广播
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.ddb.broadcasttest.MY_BROADCAST");
+                        sendBroadcast(intent);
+                    }
+                }
+        );
+
+
+//        intentFilter = new IntentFilter();
+//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//
+//        networkChangeReceiver = new NetworkChangeReceiver();
+//        registerReceiver(networkChangeReceiver,intentFilter);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+    //定义内部类的网络改变接收器
+    class NetworkChangeReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent){
+            //Toast.makeText(context,"network changes",Toast.LENGTH_SHORT).show();
+
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo !=null && networkInfo.isAvailable()){
+                Toast.makeText(context,"Network is available",Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(context,"Network is unavailable",Toast.LENGTH_SHORT).show();
+
+            }
+
+
+        }
+
+    }
+
+}
